ea4c671dba4ff1b0e8a9f6f51339653e9cb8e067
ZUITOPIA <korhwngml123@naver.com>
2022-10-16 16:34:41 +0900

    004

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import ImportComponent from "./R003_ImportComponent";
+import LifecycleEx from "./R004_LifecycleEx";
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <div>
         <h1>Start React 200!</h1>
         <p>CSS 적용하기</p>
-        <ImportComponent />
+        <LifecycleEx />
       </div>
     </>
   );
