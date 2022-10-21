08fb9e1525d3fe297e26fffb30d32a17880771e3
ZUITOPIA <korhwngml123@naver.com>
2022-10-21 23:34:54 +0900

    019 : PropsBoolean

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,13 @@
 import React from "react";
 import "./App.css";
-import PropsDataType from "./R018_PropsDataType";
+import PropsBoolean from "./R019_PropsBoolean";
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <PropsDataType
-        String="react"
-        Number={200}
-        Boolean={1 == 1}
-        Array={[0, 1, 8]}
-        ObjectJson={{ react: "리액트", twohundred: "200" }}
-        Function={console.log("FunctionProps : function!")}
-      />
+      <PropsBoolean BooleanTrueFalse={false} />
     </div>
   );
 }
